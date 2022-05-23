803d3c6534d1c9568d61455040abb4d2db6bde1d
kekwem <93526125+kekwem@users.noreply.github.com>
2022-05-23 14:21:19 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Collections.ObjectModel;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ObservableCollection<Person> lista_osob = new ObservableCollection<Person>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listBox.ItemsSource = lista_osob;
+        }
+      
+        private void dodajButton_Click(object sender, RoutedEventArgs e)
+        {
+            double temp_double;
+            int temp_int;
+            if (String.IsNullOrEmpty(nameTextBox.Text) || String.IsNullOrEmpty(surnameTextBox.Text) ||
+                String.IsNullOrEmpty(ageTextBox.Text) || String.IsNullOrEmpty(weightTextBox.Text))
+            {
+                MessageBox.Show("Trzeba wpisac dane we wszystkie pola!");
+            }
+            else if (!Double.TryParse(weightTextBox.Text, out temp_double) || !int.TryParse(ageTextBox.Text, out temp_int))
+            {
+                MessageBox.Show("Nieprawidlowy format wprowadzonych danych!");
+            }
+            else
+            {
+                lista_osob.Add(new Person(nameTextBox.Text, surnameTextBox.Text, temp_int, temp_double));
+            }
+        }
+
+        private void usunButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Kurwa");
+            }
+            else
+            {
+                lista_osob.RemoveAt(listBox.SelectedIndex);
+            }
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    class Person
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Surname
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public double Weight
+        {
+            get;
+            set;
+        }
+
+        public Person(string name, string surname, int age, double weight)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Weight = weight;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
